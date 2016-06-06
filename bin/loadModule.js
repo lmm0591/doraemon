e5e6e455ce8012b49b5a46c97ade6b980d20a6d2
李明敏 <314407103@qq.com>
2016-06-06 16:15:19 +0800

    优化模块加载
    不需要修改路由文件

--- bin/loadModule.js
+++ bin/loadModule.js
@@ -3,10 +3,14 @@ var FS = require('fs');
 var inquirer = require('inquirer');
 
 var PATH = pathJoin('./src/routes/');
-var WEBPACK_PATH = './webpack/module.js';
+var MODTMPL_PATH = pathJoin('./node_modules/nd-doraemon/bin/') + 'mod.template';
+var WEBPACK_PATH = process.cwd() +  pathJoin('/src/module.js');
+var fileList = getFileList(PATH);
+var modTmpl = FS.readFileSync(MODTMPL_PATH,'utf8')
 
 function isDisableFile(fileName){
-  return /\.bak$/.test(fileName)
+  fileName = fileName.replace(/(\.js)$/,'').replace(/^\d+/,'')
+  return require(WEBPACK_PATH).excludeModule.indexOf(fileName) >= 0
 }
 
 //获取路由文件列表
@@ -17,98 +21,61 @@ function getFileList(dir){
   })
 }
 
-//禁用路由
-function disableFile(fileName){
-  var oldFilePath = pathJoin(PATH,fileName)
-  //设置该文件不可被加载
-  var newFilePath = pathJoin(PATH,fileName.replace(/.js$/,'.bak'))
-  try{
-    FS.renameSync(oldFilePath, newFilePath)
-  } catch(e) {
-    console.log(e)
-  }
-}
-
-//启用路由
-function ableFile(fileName){
-  var oldFilePath = pathJoin(PATH,fileName)
-  //设置该文件可被加载
-  var newFilePath = pathJoin(PATH,fileName.replace(/\.bak$/,'.js'))
-  try{
-    FS.renameSync(oldFilePath, newFilePath)
-  } catch(e) {
-    console.log(e)
-  }
-}
-
-// 控制台生成结果报告
-function buildReport(){
-  //输出可加载的模块
-  getFileList(PATH).forEach(function(fileName){
-    isDisableFile(fileName) || console.log('[%s] 模块可加载!'.info, fileName.replace(/\.js$/,''))
-  })
-  console.log('设置完成!'.info );
-}
-
 //生成 WEBPACK 模块加载文件
-function buildExcludeModules(modules){
+function buildExcludeModules(modules, flag){
   var data = modules.map(function(fileName){
     //除去文件后缀和前缀
-    return '/app\\' + fileName.replace(/(\.bak|\.js)$/,"/").replace(/^\d+/,"")
+    return fileName.replace(/(\.js)$/,'').replace(/^\d+/,'')
   })
-  var loadConfig = {
-    excludeModule : data
-  }
-  // /\"\/([\w|\\]+)\/\"/g,"\/$1\/" 把字符串转成正则
-  // 如： "/app\\shop/" => /app\\shop/
-  FS.writeFileSync(WEBPACK_PATH,"export default " + JSON.stringify(loadConfig,null,2).replace(/\"\/([\w|\\]+)\/\"/g,"\/$1\/"))
-  console.log('生成 webpack 配置文件！')
+
+  var json = JSON.stringify(data).replace(/\"/g,"'");
+  var result = modTmpl.replace('{excludeModule}',json);
+  FS.writeFileSync(WEBPACK_PATH, result)
 }
+
 
 module.exports = function(routePath,command) {
 
   routePath && (PATH=routePath)
-  var fileList = getFileList(PATH)
 
   //来移除，加载所有的模块
   if(command.noRemove){
-    fileList.forEach(function(fileName){
-      isDisableFile(fileName) && ableFile(fileName);
-    })
-    buildReport()
     buildExcludeModules([])
   }else{
 
-    choiceList = [];
-    //生成模块选择列表
-    fileList.forEach(function(fileName){
-      choiceList.push({name:fileName.replace(/(.js|.bak)$/,''),value:fileName,checked:isDisableFile(fileName)})
-    })
-
-    var questions = [{
-      type: 'checkbox',
-      name: 'excludeModules',
-      message: '选择要移除的模块?',
-      choices: choiceList
+    var loadMode = [{
+      type: 'list',
+      name: 'loadMode',
+      message: '选择加载方式?',
+      choices: [{name:'加载模块',value: true},{name:'移除模块',value: false}]
     }];
 
-    inquirer.prompt(questions).then(function(answers) {
-      var fileList = getFileList(PATH)
-
-      buildExcludeModules(answers.excludeModules)
-
+    inquirer.prompt(loadMode).then(function(answers) {
+      var isLoadMode = answers.loadMode;
+      var choiceList = [];
+      //生成模块选择列表
       fileList.forEach(function(fileName){
-        //要被移除 "加载文件"
-        if(answers.excludeModules.indexOf(fileName) >= 0 && isDisableFile(fileName) === false ){
-          disableFile(fileName);
-
-        //恢复 "加载文件"
-      } else if (answers.excludeModules.indexOf(fileName) === -1 && isDisableFile(fileName) ){
-          ableFile(fileName);
-        }
+        var checked = isLoadMode ? false : isDisableFile(fileName)
+        choiceList.push({name:fileName.replace(/(\.js)$/,''),value:fileName, checked: checked})
       })
-      buildReport()
-    })
 
+      var questions = [{
+        type: 'checkbox',
+        name: 'excludeModules',
+        message: isLoadMode ? '选择要加载的模块?' : '选择要移除的模块?',
+        choices: choiceList
+      }];
+
+      inquirer.prompt(questions).then(function(answers) {
+        var excludeModules = answers.excludeModules
+        if(isLoadMode){//如果是加载模式，就取反
+          buildExcludeModules(fileList)
+          excludeModules = fileList.filter(function(e){
+            return excludeModules.indexOf(e) == -1
+          })
+        }
+        buildExcludeModules(excludeModules)
+      })
+    })
   }
 }
